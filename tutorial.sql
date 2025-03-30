b4d1904ec6ad8747f70fc59618025f9565962810
tejfaster <tejfaster55@gmail.com>
2025-03-30 15:41:31 +0200

    tutorial add

---
+++ tutorial.sql
@@ -0,0 +1,41 @@
+-- CREATE TABLE person(
+-- id INT,
+-- name VARCHAR(100),
+-- city VARCHAR(100)
+-- );
+
+-- INSERT INTO person(id,name,city)
+-- VALUES
+-- (102,'Laddu','Siwan'),
+-- (103,'Tushar','Rohini');
+
+-- SELECT name , city FROM person;
+
+-- UPDATE person
+-- SET city = 'Laxmi Nagar'
+-- WHERE id=102;
+
+-- SELECT * FROM person;
+
+-- DELETE FROM person
+-- WHERE name='Laddu';
+
+-- SELECT * FROM person;
+
+-- INSERT INTO person
+-- VALUES
+-- (101,'Ruby','Sonipath');
+
+-- SELECT * FROM person;
+
+-- INSERT INTO person(id,name)
+-- Values
+-- (105,'Rahul');
+
+-- SELECT * FROM person;
+
+-- UPDATE person
+-- SET id = 104
+-- WHERE name = 'Rahul';
+
+SELECT * FROM person;
